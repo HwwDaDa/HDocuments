44e2513d2700be75af331ae844d0a14259d39bc4
Admin <tjuthww@163.com>
2017-06-14 11:12:51 +0800

    no message

---
+++ test2/test2/AppDelegate.h
@@ -0,0 +1,17 @@
+//
+//  AppDelegate.h
+//  test2
+//
+//  Created by Admin on 2017/6/13.
+//  Copyright © 2017年 Admin. All rights reserved.
+//
+
+#import <UIKit/UIKit.h>
+
+@interface AppDelegate : UIResponder <UIApplicationDelegate>
+
+@property (strong, nonatomic) UIWindow *window;
+
+
+@end
+
